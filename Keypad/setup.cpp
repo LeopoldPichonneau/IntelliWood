a116aabb0b67e1f4f9596bad75d803781bba714d
KPPHC <178666444+KPPHC@users.noreply.github.com>
2026-05-21 14:11:19 +0300

    fix: buzzer pin

--- Keypad/setup.cpp
+++ Keypad/setup.cpp
@@ -1,21 +1,2 @@
-pwm(alarm, 33, 2000);
+pwm(alarm, 39, 2000);
 mpr121(cap).i2c(5,6);
-
-void setup_devices() {
-    // BIP de test au démarrage
-    alarm.on(); 
-    delay(1000);
-    alarm.off();
-    
-    // Allumer quand on appuie
-    cap.on_press(0, [&](Device& d) {
-        alarm.on();
-        return true;
-    });
-
-    // ÉTEINDRE quand on relâche (sinon ça sonne à l'infini !)
-    cap.on_release(0, [&](Device& d) {
-        alarm.off();
-        return true;
-    });
-}
